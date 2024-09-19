171efc4a0ebcfa7056543dc5e7655f954ce66aad
Adam Sobkowiak <Adamsky_biznes@o2.pl>
2024-09-19 20:40:00 +0200

    temp

--- WinLoseCounter/winlosecounter.js
+++ WinLoseCounter/winlosecounter.js
@@ -1,9 +1,5 @@
 let fieldData, apiToken, text;
-let timerPauseCommands, timerStartCommands, timerStopCommands, timerResetCommands = [];
-
-let seconds = 0;
-let timerInterval;
-let isRunning = false;
+let wlCounterStartCommands, wlCounterStopCommands, winCounter, loseCounter = [];
 
 const checkPrivileges = (data, privileges) => {
     const {tags, userId} = data;
@@ -20,23 +16,18 @@ function handleTimer(obj) {
     let data = obj.detail.event.data;
     const { text } = data;
 
-    const startCommand = timerStartCommands.split(',').filter(s => !!s).map(s => s.trim());
-    const stopCommand = timerStopCommands.split(',').filter(s => !!s).map(s => s.trim());
-    const resetCommand = timerResetCommands.split(',').filter(s => !!s).map(s => s.trim());
+    const startCommand = wlCounterStartCommands.split(',').filter(s => !!s).map(s => s.trim());
+    const stopCommand = wlCounterStopCommands.split(',').filter(s => !!s).map(s => s.trim());
 
     const commandThatMatchesStart = startCommand.find(command => text.toLowerCase().startsWith(command.toLowerCase()));
     const commandThatMatchesStop = stopCommand.find(command => text.toLowerCase().startsWith(command.toLowerCase()));
-    const commandThatMatchesReset = resetCommand.find(command => text.toLowerCase().startsWith(command.toLowerCase()));
+    
 
     if (commandThatMatchesStart && checkPrivileges(data)) {
-        startTimer();
+        showCounter();
     }
     if (commandThatMatchesStop && checkPrivileges(data)) {
-        pauseTimer();
-    }
-
-    if (commandThatMatchesReset && checkPrivileges(data)) {
-        resetTimer();
+        hideCounter();
     }
 };
 
@@ -59,8 +50,28 @@ window.addEventListener('onWidgetLoad', function (obj) {
     fieldData = obj.detail.fieldData;
     apiToken = obj.detail.channel.apiToken;
 
-    timerStartCommands = fieldData['timerStartCommands'];
-    timerStopCommands = fieldData['timerStopCommands'];
-    timerPauseCommands = fieldData['timerPauseCommands'];
-    timerResetCommands = fieldData['timerResetCommands'];
+    wlCounterStartCommands = fieldData['wlCounterStartCommands'];
+    wlCounterStopCommands = fieldData['wlCounterStopCommands'];
+    winCounter = fieldData['winCounter'];
+    loseCounter = fieldData['loseCounter'];
 });
+
+function showCounter(){
+    document.getElementById("container").style.visibility = "visible";
+      SE_API.counters.get(winCounter).then(counter => {
+        // counter is of the format { counter, value }
+      	tempv = counter.value.toString();
+      	console.log(tempv);
+        document.getElementById("wintext").innerHTML = tempv;
+    });
+    SE_API.counters.get(loseCounter).then(counter => {
+        // counter is of the format { counter, value }
+        tempc = counter.value.toString();
+      	console.log(tempc);
+        document.getElementById("losetext").innerHTML = tempc;
+    });
+};
+
+function hideCounter(){
+    document.getElementById("container").style.visibility = "hidden";
+};
